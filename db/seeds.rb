23cb36e0682c5361c6719d03b15eba4fa18b1e0c
Pablo Donalisio <pablo.donalisio@gmail.com>
2022-08-11 19:50:31 -0300

    add products to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+  currency = n.even? ? 'ARS' : 'USD'
+  Product.create(name: "product #{n}",
+                 currency: currency,
+                 price: rand(100),
+                 quantity: rand(5))
+end
